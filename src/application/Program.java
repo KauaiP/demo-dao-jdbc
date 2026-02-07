f655a8456776f17ee536949ded76f2144c337392
KauaiP <kauaipereira@id.uff.br>
2026-02-07 19:11:38 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -12,12 +13,10 @@ public class Program {
     public static void main (String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Department department = new Department(2, null);
 
-       Department department = new Department(2, null);
-       List<Seller> list = sellerDao.findByDepartment(department);
-
-       for(Seller obj : list){
-           System.out.println(obj);
-       }
+      Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
+      sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id = " + newSeller.getId());
     }
 }
